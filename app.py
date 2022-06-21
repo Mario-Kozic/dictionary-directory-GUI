7a54d9137a146bc55a15e0e3cd0eb7a5454e5130
Mario Kozic <mkozic89@gmail.com>
2022-06-21 15:54:05 -0400

    importing our modules

---
+++ app.py
@@ -0,0 +1,17 @@
+
+import json
+from difflib import get_close_matches
+from tkinter import *
+from tkinter import messagebox
+
+
+data = json.load(open('data.json'))
+
+root = Tk()
+root.title('Dictionary')
+
+
+
+
+
+root.mainloop()
